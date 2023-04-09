071280f350a7602737129d0b1e42ff627ffa66c5
Caique de Castro <caique.castro@venturus.org.br>
2023-04-09 20:36:08 -0300

    feat: Toggle profile menu on navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import logo from "~/assets/logo.svg";
 import { Link } from "~/app/components/Link";
+import { ProfileMenu } from "./ProfileMenu";
 
 export function Navbar() {
   return (
@@ -78,7 +79,7 @@ export function Navbar() {
                 </Link>
 
                 <Link
-                  href="#"
+                  href="/professionals"
                   className="rounded-md px-3 py-2 text-sm font-medium"
                   defaultClassName="text-gray-300 hover:bg-gray-700 hover:text-white"
                   activeClassName="bg-gray-900 text-white"
@@ -110,72 +111,7 @@ export function Navbar() {
               </svg>
             </button>
 
-            {/* <!-- Profile dropdown --> */}
-            <div className="relative ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span className="sr-only">Open user menu</span>
-                  {/* <Image
-                    className="h-8 w-8 rounded-full"
-                    src="#"
-                    alt=""
-                  /> */}
-                </button>
-              </div>
-
-              {/* <!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          --> */}
-              <div
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-                tabIndex={-1}
-              >
-                {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-0"
-                >
-                  Perfil
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-1"
-                >
-                  Configurações
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex={-1}
-                  id="user-menu-item-2"
-                >
-                  Sair
-                </a>
-              </div>
-            </div>
+            <ProfileMenu />
           </div>
         </div>
       </div>
