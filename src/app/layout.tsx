f954b6525ebaeddf5e938c14b94e091177edfea3
Caique de Castro <caique.castro@venturus.org.br>
2023-04-11 09:49:41 -0300

    feat: add content on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
+import { PropsWithChildren } from "react";
+import { Inter } from "next/font/google";
 import { Navbar } from "~/app/components/Navbar";
 import "./globals.css";
-import { PropsWithChildren } from "react";
 
 export const metadata = {
   title: "Skills Catalog",
   description: "Catalog for professional skills",
 };
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.className} bg-slate-50`}>
         <Navbar />
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Text, Title } from "@tremor/react";
 
 export default function Home() {
-  return <main>
-    <form></form>
-  </main>;
+  return (
+    <main className="p-6 sm:p-10">
+      <Title>Dashboard</Title>
+      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+    </main>
+  );
 }
