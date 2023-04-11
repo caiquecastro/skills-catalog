b17db97e46fdc76d91c6765dd8bab4c0ec186e06
Caique de Castro <caique.castro@venturus.org.br>
2023-04-11 09:27:13 -0300

    feat: add page to list skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,73 @@
 const SkillsPage = () => {
   return (
     <>
-      <h1>Skills</h1>
+      <h1 className="text-4xl mb-3 font-semibold">Habilidades</h1>
+
+      <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+        <thead className="bg-gray-50">
+          <tr>
+            <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+              Nome
+            </th>
+            <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+              Quantidade de profissionais
+            </th>
+            <th scope="col" className="px-6 py-4 font-medium text-gray-900"></th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+          <tr>
+            <th className="px-6 py-4 font-medium text-gray-900">React.js</th>
+            <td className="px-6 py-4">10</td>
+            <td className="flex justify-end gap-4 px-6 py-4 font-medium">
+              <a href="">Excluir</a>
+              <a href="" className="text-primary-700">
+                Editar
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th className="px-6 py-4 font-medium text-gray-900">Java</th>
+            <td className="px-6 py-4">50</td>
+            <td className="flex justify-end gap-4 px-6 py-4 font-medium">
+              <a href="">Excluir</a>
+              <a href="" className="text-primary-700">
+                Editar
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th className="px-6 py-4 font-medium text-gray-900">Helen Howard</th>
+            <td className="px-6 py-4">Nov.4 2022</td>
+            <td className="flex justify-end gap-4 px-6 py-4 font-medium">
+              <a href="">Excluir</a>
+              <a href="" className="text-primary-700">
+                Editar
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th className="px-6 py-4 font-medium text-gray-900">Helen Howard</th>
+            <td className="px-6 py-4">Nov.4 2022</td>
+            <td className="flex justify-end gap-4 px-6 py-4 font-medium">
+              <a href="">Excluir</a>
+              <a href="" className="text-primary-700">
+                Editar
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th className="px-6 py-4 font-medium text-gray-900">.NET</th>
+            <td className="px-6 py-4">40</td>
+            <td className="flex justify-end gap-4 px-6 py-4 font-medium">
+              <a href="">Excluir</a>
+              <a href="" className="text-primary-700">
+                Editar
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
