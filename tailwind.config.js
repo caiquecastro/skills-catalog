51c64f3e0aaf5e50680cda5fabfd006ae14eb237
Caique de Castro <castro.caique@gmail.com>
2023-04-26 22:10:55 -0300

    fix: remove unused extended bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,7 @@ module.exports = {
     "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
